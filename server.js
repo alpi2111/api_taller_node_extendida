ecf2217562529253561a0558f56bcb7260d9f157
Carlos Alpirez <carlos.alpi21@gmail.com>
2020-11-19 22:00:19 -0600

    Se cambio el backend de agregar nota, responde a los diferentes status code

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const lodash_id = require('lodash-id');
 const lowdb = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
 const body_parser = require('body-parser');
@@ -8,43 +9,65 @@ const nanoid = require('nanoid');
 const archivo = new FileSync("database.json");
 const db = lowdb(archivo);
 
-db.defaults({
-    usuarios: [],
-    notas: []
-});
+db._.mixin(lodash_id);
+
+const notas = db.defaults({ notas: [] }).get('notas');
+const usuarios = db.defaults({ usuarios: [] }).get('usuarios');
 
 const server = express();
 server.use(cors());
 
 server.use(body_parser.json());
 
+const PORT = process.env.PORT || 3001;
+
 // ?? GET POST PUT DELETE UPDATE
 
 server.get("/", (request, variable) => {
-    variable.status(200).json({"mensaje": 'Hola mundo en node API REST'});
-    // ? 404 
+    variable.status(200).json({ "mensaje": 'Hola mundo en node API REST' });
 });
 
 server.get("/notas", (req, res) => {
     res.status(200).json(
-        db.get("notas").values()
+        notas.values()
     )
 });
 
-server.get("/usuario", (req, res) => {
+server.get("/usuarios", (req, res) => {
     res.status(200).json(
-        db.get("usuarios").values()
+        usuarios.values()
     )
 });
 
-server.post("/agregarNota", (req, res) => {
-    const tituloNota = req.body.titulo_nota;
-    db.set("notas", {
-        "titulo_nota": tituloNota,
-        "id": nanoid.nanoid(),
-    }).write();
-
-    res.status(200).send('Todo ha salido correcto');
+server.post("/agregarNota", async (req, res) => {
+    if (req.body.titulo_nota === undefined || req.body.mensaje === undefined) {
+        res.status(400).json({
+            status_code: 400,
+            mensaje: "Error, faltan parámetros para realizar la petición",
+            parametros_faltantes: {
+                titulo_nota: req.body.titulo_nota === undefined ? null : req.body.titulo_nota,
+                mensaje: req.body.mensaje === undefined ? null : req.body.mensaje
+            }
+        });
+    } else {
+        if (Object.keys(req.body).length > 2) {
+            res.status(413).json({
+                status_code: 413,
+                mensaje: "Demasiados parámetros de los necesarios",
+                aceptados: ["titulo_nota", "mensaje"]
+            });
+        } else {
+            notas.insert({
+                id: nanoid.nanoid(),
+                titulo_nota: req.body.titulo_nota,
+                mensaje: req.body.mensaje
+            }).write();
+            res.status(200).json({
+                status_code: 200,
+                mensaje: 'Todo ha salido correcto'
+            });
+        }
+    }
 });
 
 server.get("/agregarUsuario", (req, res) => {
@@ -60,7 +83,9 @@ server.get("/agregarUsuario", (req, res) => {
     res.status(200).send('Usuario ha salido correcto');
 });
 
-server.listen(3000, () => console.log('Server en localhost:3000'));
+// process.env.
+// console.log(process);
+server.listen(PORT, () => console.log(`Server en puerto:${PORT}`));
 
 
 // ??
